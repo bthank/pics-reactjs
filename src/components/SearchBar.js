d89a60af950dc721fdeec9402af8aa53a28a25f7
bthank <bthankachan@yahoo.com>
2021-09-14 18:41:05 -0400

    Correcting this errors

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,19 +4,23 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+    state = { term: ''};
 
-  
+    // event handler for form submission
+    onFormSubmit = (event) => {
+        // this line will keep the browser from trying to submit the form automatically on enter and in the process refresh the page
+        // always add this to every project form I create; we never want a form to submit automatically on enter
+        event.preventDefault();
+        console.log(this.state.term);
+    }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit}  className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} />
+                        <input type="text" value={this.state.term}  onChange={(e) => this.setState({term: e.target.value})} />
                     </div> 
                 </form>
             </div>
