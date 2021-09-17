935608cfa455c5fdb1d8ff30b583b54f0a10e48a
bthank <bthankachan@yahoo.com>
2021-09-16 23:17:25 -0400

    Added display logic

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,19 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 
 const ImageList = (props) => {
     console.log('In ImageList.js: ', props.images);
+    // destructure off image into 'description, id and urls' properties
     const images = props.images.map((image) => {
-        return <img src={image.urls.regular} />
+        return  <div>
+                    <ImageCard key={image.id} image={image} />  
+                </div>;
+    
     });
 
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
